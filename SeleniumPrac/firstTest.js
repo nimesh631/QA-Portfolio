71f619942de4a18643c6f60b1d94c514f30f48fa
nimesh631 <nimeshkayastha067@gmail.com>
2025-10-21 08:12:37 +0545

    feat: assertion using node and chai should

---
+++ SeleniumPrac/firstTest.js
@@ -0,0 +1,32 @@
+const {Builder, By, Key} = require("selenium-webdriver");
+const assert = require("assert");
+var should = require("chai").should();
+
+
+async function  example() {
+    
+    //launch the browser
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    // navigate to our application
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+    //add todo
+    await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+    //assert
+    let todoText = await driver.findElement(By.xpath("//ul/li[last()]")).getText().then(function(value){
+        return value;
+    });
+
+    //assert using node assertion
+    // assert.strictEqual(todoText,"Learn Selenium");
+
+    //assert using chai should
+    todoText.should.equal("Learn Selenium");
+
+    //close the browser
+    await driver.quit();
+}
+
+example();
